4ec70221d1b2f4b3fa6535714aef8cc2687a8c72
nss27 <ghtjd8582@gmail.com>
2022-08-18 17:24:39 +0900

    프로젝트 초기화

--- utils/common.js
+++
@@ -1,9 +0,0 @@
-export const isNull = data => data == undefined || data == null || data == ''
-export const rootPath = () => {
-    switch(window.location.host) {
-        case 'nss27.github.io':
-            return `${window.location.origin}/JCU`
-        default:
-            return window.location.origin
-    }
-}
