198cea2defbe8ab14151f0adabc2381b91ea6c9b
nss27 <ghtjd8582@gmail.com>
2022-09-02 18:04:31 +0900

    플레이어 정보 표출 구현중

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,74 @@
 <template>
     <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-buttons>
+                    <ion-menu-button></ion-menu-button>
+                </ion-buttons>
+                <ion-title>전사연</ion-title>
+            </ion-toolbar>
+            <ion-toolbar>
+                <ion-searchbar placeholder="닉네임을 입력하세요" v-model="player" @keypress="enter($event)"></ion-searchbar>
+                <ion-buttons slot="end">
+                    <ion-button fill="clear" @click="playerSearch()" id="search-btn">
+                        <ion-icon :icon="searchSharp" slot="icon-only"></ion-icon>
+                    </ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true" id="main">
+            <ion-list lines="none" v-if="playerInfo">
+                <ion-item>
+                    <ion-grid class="no-padding">
+                        <ion-row v-if="playerInfo.clanName">
+                            <ion-col>
+                                <ion-text color="warning">
+                                    <small>{{ playerInfo.clanName }}</small>
+                                </ion-text>
+                            </ion-col>
+                        </ion-row>
+                        <ion-row class="ion-align-items-center">
+                            <ion-col size="10">
+                                <ion-text>
+                                    <h1 class="ion-no-margin">{{ playerInfo.nickname }}</h1>
+                                </ion-text>
+                            </ion-col>
+                            <ion-col size="2" class="ion-text-end">
+                                <ion-text>{{ playerInfo.grade }}급</ion-text>
+                            </ion-col>
+                        </ion-row>
+                    </ion-grid>
+                </ion-item>
+                <ion-item>
+                    <ion-label>일반전 승률</ion-label>
+                    <ion-note :color="gameNormalRatioColor">{{ `${gameNormalRatio}%` }}</ion-note>
+                </ion-item>
+                <ion-item>
+                    <ion-label>공식전 승률</ion-label>
+                </ion-item>
+                <ion-item v-if="playerInfo.tierName">
+                    <ion-grid>
+                        <ion-row class="ion-text-center ion-align-items-center">
+                            <ion-col size="6">
+                                <ion-text color="primary">
+                                    <h1 class="ion-no-margin">{{ playerInfo.tierName }}</h1>
+                                </ion-text>
+                            </ion-col>
+                            <ion-col size="3">
+                                <div>현재점수</div>
+                                <div>{{ playerInfo.ratingPoint }}</div>
+                            </ion-col>
+                            <ion-col size="3">
+                                <div>최고점수</div>
+                                <div>{{ playerInfo.maxRatingPoint }}</div>
+                            </ion-col>
+                        </ion-row>
+                    </ion-grid>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+
         <ion-menu content-id="main" menu-id="main-menu">
             <ion-header>
                 <ion-toolbar>
@@ -9,48 +78,15 @@
 
             <ion-content>
                 <ion-list>
+                    <ion-item button>
+                        사이퍼즈 캐릭터상 테스트
+                    </ion-item>
                     <ion-item button router-link="/restaurantListMap">
                         맛집리스트
                     </ion-item>
                 </ion-list>
             </ion-content>
         </ion-menu>
-
-        <ion-header :translucent="true">
-            <ion-toolbar>
-                <ion-buttons>
-                    <ion-menu-button></ion-menu-button>
-                </ion-buttons>
-                <ion-title>전사모</ion-title>
-            </ion-toolbar>
-        </ion-header>
-
-        <ion-content :fullscreen="true" id="main">
-            <ion-list>
-                <ion-item>
-                    <ion-searchbar placeholder="닉네임을 입력하세요" v-model="player"></ion-searchbar>
-                    <ion-button fill="clear" @click="playerSearch()">
-                        <ion-icon :icon="searchSharp" slot="icon-only"></ion-icon>
-                    </ion-button>
-                </ion-item>
-                <ion-item v-if="playerInfo">
-                    <ion-label class="ion-text-wrap">
-                        <h1>
-                            {{ playerInfo.nickname }}
-                            <small>
-                                {{ playerInfo.grade }}급
-                            </small>
-                            <small v-if="playerInfo.clanName">
-                                클랜: {{ playerInfo.clanName }}
-                            </small>
-                        </h1>
-                        <h1>랭킹</h1>
-                        <h3>{{ playerInfo.tierName }}</h3>
-                        <p>{{ playerInfo.ratingPoint }} / {{ playerInfo.maxRatingPoint }}</p>
-                    </ion-label>
-                </ion-item>
-            </ion-list>
-        </ion-content>
     </ion-page>
 </template>
 
@@ -72,9 +108,14 @@ import {
     IonButton,
     IonIcon,
     loadingController,
-    IonLabel
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonText,
+    IonNote
 } from '@ionic/vue';
-import { defineComponent, ref } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import { searchSharp } from 'ionicons/icons';
 import NeopleApi from '@/utils/NeopleApi';
 import Common from '@/utils/Common';
@@ -94,7 +135,12 @@ export default defineComponent({
         IonMenuButton,
         IonButton,
         IonIcon,
-        IonLabel
+        IonLabel,
+        IonGrid,
+        IonRow,
+        IonCol,
+        IonText,
+        IonNote
     },
     setup() {
         const player = ref('');
@@ -112,15 +158,50 @@ export default defineComponent({
 
             if (Common.isNull(playerIdJson.rows)) {
                 playerId = '';
+                playerInfo.value = ref();
             } else {
                 playerId = playerIdJson.rows[0].playerId;
                 playerInfo.value = ref(await NeopleApi.cyPlayerInfo({ playerId: playerId })).value;
                 console.log(playerInfo.value);
-
             }
 
             loading.dismiss();
         };
+
+        const enter = (e: KeyboardEvent) => {
+            if (e.key === 'Enter') {
+                setTimeout(() => {
+                    const btn = document.querySelector('#search-btn') as HTMLElement;
+                    btn.click();
+                }, 250);
+            }
+        };
+
+        const gameNormalRatio = computed(() => {
+            let ratio = 0;
+
+            if (!Common.isNull(playerInfo) && !Common.isNull(playerInfo.value.records)) {
+                const game = (playerInfo.value.records as any[]).filter(game => game.gameTypeId === 'normal')[0];
+                ratio = Math.round(game.winCount / (game.winCount + game.loseCount) * 100);
+            }
+
+            return ratio;
+        });
+
+        const gameNormalRatioColor = computed(() => {
+            let color = '';
+
+            if (gameNormalRatio.value >= 75) {
+                color = 'primary';
+            } else if (gameNormalRatio.value >= 50) {
+                color = 'success';
+            } else if (gameNormalRatio.value >= 25) {
+                color = 'warning';
+            } else {
+                color = 'danger';
+            }
+            return color;
+        });
 
         onIonViewDidLeave(() => {
             menuController.close('main-menu');
@@ -130,11 +211,16 @@ export default defineComponent({
             searchSharp,
             player,
             playerSearch,
-            playerInfo
+            playerInfo,
+            enter,
+            gameNormalRatio,
+            gameNormalRatioColor
         }
     }
 });
 </script>
-
 <style scoped>
+.no-padding ion-col {
+    --ion-grid-column-padding: 0;
+}
 </style>
